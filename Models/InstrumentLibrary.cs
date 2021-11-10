025902c0baf1ebe9ba52d538a0b851d354616f2c
Emmeran Darribere <24435@student.dorset-college.ie>
2021-11-10 17:40:15 +0100

    Ajoutez des fichiers projet.

---
+++ Models/InstrumentLibrary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstrumentLibrary.Models
+{
+    public abstract class Instrument
+    {
+        public string SerialNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public Instrument(string _name)
+        {
+            Name = _name;
+        }
+
+        public abstract void Play();
+    }
+    
+    public class Guitar : Instrument
+    {
+        public Guitar(string _name) : base(_name)
+        {
+
+        }
+
+        public override void Play()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine($"I am player a {Name} Guitar");
+        }
+    }
+    public class Mandolin : Instrument
+    {
+        public Mandolin(string _name) : base(_name)
+        {
+
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"I am player a {Name} Mandolin");
+        }
+    }
+    public class Ukulele : Instrument
+    {
+        public Ukulele(string _name) : base(_name)
+        {
+
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"I am player a {Name} Ukulele");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using InstrumentLibrary.Models;
+using System;
+
+namespace InstrumentLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Guitar myGuitar = new Guitar("Gibson");
+            Mandolin myMandolin = new Mandolin("Kentucky KM-140");
+            Ukulele myUkulele = new Ukulele("Ortega");
+
+            myUkulele.SerialNumber = "abc123";
+
+            myGuitar.Play();
+            myMandolin.Play();
+            myUkulele.Play();
+        }
+    }
+}
